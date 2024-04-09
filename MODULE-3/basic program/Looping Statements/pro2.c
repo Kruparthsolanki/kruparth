116cb8f9908aa44c5d12281d201fa3e335540cc7
Kruparthsolanki <160168413+Kruparthsolanki@users.noreply.github.com>
2024-04-09 22:33:39 +0530

    Add files via upload

---
+++ MODULE-3/basic program/Looping Statements/pro2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+
+ int i,num;
+ for (i=1;i<=5;i++)
+ { 
+ printf("enter %d number:",i);
+ scanf("%d",&num);
+ printf("the number is:%d\n",num);
+ }
+ return 0;
+ }
+ 
